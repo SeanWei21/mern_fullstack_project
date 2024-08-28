4b3c670b6618032a690f1ebb08ac6fddcbf0ea26
SeanWei21 <sean.wei21@gmail.com>
2024-08-28 15:34:14 +0800

    Add feature: Express Router & API Routes

--- backend/server.js
+++ backend/server.js
@@ -4,22 +4,30 @@ require('dotenv').config()
 
 // 1. Get Express from node module
 const express= require('express')
+const workoutRoutes= require('./routes/workouts')
 
 // 2. express app
 const app= express()
 
-// Middleware
-app.use((req, res, next)=>{
-    console.log(req.path, req.method)
+// // Middleware
+// app.use((req, res, next)=>{
+//     console.log(req.path, req.method)
 
-    // Invoke 'next' arg to proceed to next code
-    next()
-})
+//     // Invoke 'next' arg to proceed to next code
+//     next()
+// })
+app.use(express.json())
 
-// routes
-app.get('/', (req, res) =>{
-        res.json({mssg: 'Welcome to the App'})
-})
+
+//// routes
+// app.get('/', (req, res) =>{
+//         res.json({mssg: 'Welcome to the App'})
+// })
+
+// Routes, uses Router from workouts.js
+// routes calls with prefix of '/api/workouts'
+app.use('/api/workouts', workoutRoutes)
+
 
 // listen for request
 // process.env.PORT = PORT number in .env file
